365993b27399e99032ede491de6e2e87e780ae40
Tsun0193 <dduy193@gmail.com>
2025-10-16 20:59:29 +0000

    Add KnowledgeGraphRAGResult and pipeline to __all__ exports

--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
 from .llm import run_llm_pipeline
+from .rag import KnowledgeGraphRAGResult, run_knowledge_graph_rag_pipeline
 from .rule_based import run_rule_based_pipeline
 
 __all__ = [
     "run_llm_pipeline",
+    "run_knowledge_graph_rag_pipeline",
+    "KnowledgeGraphRAGResult",
     "run_rule_based_pipeline",
 ]
